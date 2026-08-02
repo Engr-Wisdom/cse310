1a1ee6d89d348d4557c0395bccaad17dc4e31d1d
Wisdom Effiong <wisdomeffiong2008@gmail.com>
2026-08-02 11:33:49 +0100

    Formated StudentTaskPlanner index.tsx file with prettier

--- MovieApp/src/services/searchService.ts
+++
@@ -1,9 +0,0 @@
-import { API_KEY, BASE_URL } from "../api/apiConfig";
-
-export const searchMovies = async (query: string) => {
-  const response = await fetch(
-    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}`
-  );
-
-  return await response.json();
-};
--- MovieApp/src/types/movie.ts
+++
@@ -1,9 +0,0 @@
-export interface Movie {
-  id: number;
-  title: string;
-  overview: string;
-  poster_path: string;
-  backdrop_path: string;
-  vote_average: number;
-  release_date: string;
-}
